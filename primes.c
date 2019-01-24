34bb53a27dab6989fe1e9b69601bcb7a6ca714dc
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2019-01-24 15:47:40 +0000

    Started primes

---
+++ primes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int isprime(unsigned int test) {
+
+  unsigned int i;
+
+  for (i = 2; i < test; i++) {
+    if (test % i == 0) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+
+int main(int argc, char *argv[]) {
+
+  unsigned int test = argv[1];
+
+  if (isprime(test))
+    printf("%d is prime.\n", test);
+  else
+    printf("%d is not prime.\n", test);
+
+  printf("argc is %d\n", argc);
+  for (int i = 0; i < argc; i++)
+    printf("argv[%d] is %s\n", i, argv[i]);
+
+  return 0;
+
+}
+
